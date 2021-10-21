7386302a696239db1936d8bb7ee20e56e9b596f3
5yxar <5yx4r@mail.ru>
2021-10-21 20:57:56 +0300

    Create class CandidateService

---
+++ src/Bruhunter.Application/CandidatesService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bruhunter.Application
+{
+    public class CandidatesService
+    {
+        private static readonly List<CandidateDocument> Candidates = new List<CandidateDocument>();
+    }
+}
