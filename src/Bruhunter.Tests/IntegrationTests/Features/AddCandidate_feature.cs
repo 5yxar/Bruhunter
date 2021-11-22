2ecf449b1005f083931b5c24d5bdb904e462038d
5yxar <36841344+5yxar@users.noreply.github.com>
2021-11-22 20:02:22 +0300

    Aligned offsets in in Integrations Tests

--- src/Bruhunter.Tests/IntegrationTests/Features/AddCandidate_feature.cs
+++ src/Bruhunter.Tests/IntegrationTests/Features/AddCandidate_feature.cs
@@ -11,14 +11,15 @@ namespace Bruhunter.Tests.IntegrationTests.Feautures
         [Scenario]
         public async Task Candidate_should_be_saved_in_database()
         {
-            await Runner.AddAsyncSteps(_ => Given_candidate(GiveMe.Candidate()
-                                                                  .WithId(Guid.Empty)
-                                                                  .WithFirstName("Первое")
-                                                                  .WithSecondName("Второе")
-                                                                  .Please()),
-                                _ => When_add_candidate(),
-                                _ => Then_database_should_contain_candidate())
-                        .RunAsync();
+            await Runner.AddAsyncSteps(
+                    _ => Given_candidate(GiveMe.Candidate()
+                                               .WithId(Guid.Empty)
+                                               .WithFirstName("Первое")
+                                               .WithSecondName("Второе")
+                                               .Please()),
+                    _ => When_add_candidate(),
+                    _ => Then_database_should_contain_candidate())
+                .RunAsync();
         }
     }
 }
--- src/Bruhunter.Tests/IntegrationTests/Features/DeleteCandidate_feature.cs
+++ src/Bruhunter.Tests/IntegrationTests/Features/DeleteCandidate_feature.cs
@@ -11,15 +11,16 @@ namespace Bruhunter.Tests.IntegrationTests.Feautures
         [Scenario]
         public async Task Candidate_should_be_deleted_in_database()
         {
-            await Runner.AddAsyncSteps(_ => Given_candidate(GiveMe.Candidate()
-                                                            .WithId(Guid.Empty)
-                                                            .WithFirstName("FirstName")
-                                                            .WithSecondName("SecondName")
-                                                            .Please()),
-                                _ => When_add_candidate(),
-                                _ => When_delete_candidates(),
-                                _ => Then_database_should_not_contain_candidate())
-                               .RunAsync();
+            await Runner.AddAsyncSteps(
+                    _ => Given_candidate(GiveMe.Candidate()
+                                               .WithId(Guid.Empty)
+                                               .WithFirstName("FirstName")
+                                               .WithSecondName("SecondName")
+                                               .Please()),
+                    _ => When_add_candidate(),
+                    _ => When_delete_candidates(),
+                    _ => Then_database_should_not_contain_candidate())
+                .RunAsync();
         }
     }
 }
