64c31ca5ce81452bf88c0023a092af3f63a54b71
5yxar <36841344+5yxar@users.noreply.github.com>
2021-12-04 14:45:07 +0300

    Renamed key variable and test of ChangeCandidate_feature

--- src/Bruhunter.Tests/IntegrationTests/Features/ChangeCandidate_feature.cs
+++ src/Bruhunter.Tests/IntegrationTests/Features/ChangeCandidate_feature.cs
@@ -11,10 +11,10 @@ namespace Bruhunter.Tests.IntegrationTests.Features
     public partial class ChangeCandidate_feature : FeatureFixtureBase
     {
         [Scenario]
-        public async Task Candidate_should_be_changed_in_database()
+        public async Task Candidate_first_name_should_be_changed_in_database()
         {
             var candidateId = Guid.NewGuid();
-            var candidateFirstName = "Василий";
+            var expectedCandidateFirstName = "Василий";
 
             await Runner.AddAsyncSteps(_ => Given_candidate_in_database(
                                                 GiveMe.Candidate()
@@ -23,9 +23,9 @@ namespace Bruhunter.Tests.IntegrationTests.Features
                                        _ => When_changed_candidate(
                                                 GiveMe.Candidate()
                                                 .WithId(candidateId)
-                                                .WithFirstName(candidateFirstName)
+                                                .WithFirstName(expectedCandidateFirstName)
                                                 .Please()),
-                                      _ => Then_candidate_first_name_in_database_should_be(candidateFirstName))
+                                      _ => Then_candidate_first_name_in_database_should_be(expectedCandidateFirstName))
 
                         .RunAsync();
         }
